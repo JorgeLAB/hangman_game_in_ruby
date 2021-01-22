0709bd7c45d8205721c98269e6d826074bee9f3f
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-01-22 16:21:07 -0300

    Control structure implementation for verify if the kick appear in any position in the secret word.

--- hangman.rb
+++ hangman.rb
@@ -31,10 +31,28 @@ def play(name)
 		kick = give_one_kick kicks, errors
 
 		kick_one_letter = kick.size == 1
+		
 		if kick_one_letter
+			total_find = 0
+
+			for i in 0..(secret_word.size - 1)
+				letter = secret_word[i]
+				if kick == letter
+					total_find += 1
+				end
+			end 
+
+			if total_find > 0 
+				puts "It was found #{total_find} letters." 
+			else
+				puts "The letter was not found."
+				errors += 1
+			end
 
 		else
+
 			correct_word = kick == secret_word
+		
 			if correct_word
 				puts "Congratulations, you win."
 				points_until_now += 100
@@ -44,11 +62,13 @@ def play(name)
 				errors += 1
 				points_until_now -= 30
 			end
-		end
+		
+		end	
 		kicks << kick
+	
 	end
-	puts "You receive #{ points_until_now } points."
 
+	puts "You receive #{ points_until_now } points."
 end
 
 def give_one_kick(kicks, errors)
