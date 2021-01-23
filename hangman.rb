cbb5c4307aa5c863c2526b6ef8981e4b4ab2d1f7
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-01-22 22:12:32 -0300

    Verification repeat letter was isolated the main program

--- hangman.rb
+++ hangman.rb
@@ -7,28 +7,24 @@ def play(name)
 
   while errors < 5
 
-    kick = give_one_kick kicks, errors
-    kick_one_letter = kick.size == 1
-    repeated_letter? = kicks.include? kick
+    kick = give_one_kick_valid kicks, errors
 
-    if repeated_letter?
-      repeated_letter_response
-      next
-    end
+    kick_one_letter = kick.size == 1
 
     if kick_one_letter
 
       count_letters = secret_word.count(kick)
       if count_letters > 0
-        letter_found kick count_letters
+        letter_found(kick, count_letters)
       else
-        not_found_letter
+        letter_not_found
         errors += 1
       end
 
     else
 
       correct_word = kick == secret_word
+
       if correct_word
         word_correct
         points_until_now += 100
@@ -40,9 +36,21 @@ def play(name)
       end
 
     end
-
     kicks << kick
   end
-
   result_game points_until_now
+end
+
+def give_one_kick_valid(kicks, errors)
+
+  loop do
+    kick = give_one_kick(kicks, errors)
+
+    if kicks.include? kick
+      repeated_letter_response
+    else
+      return kick
+    end
+  end
+
 end
